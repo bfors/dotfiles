08213abe4562add879d843de4cc319ab386e0eaa
Brian Forsberg <bforsberg15@gmail.com>
2024-01-11 11:13:59 -0600

    Fix init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,23 +1,5 @@
 --[[
 
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, understand
-  what your configuration is doing, and modify it to suit your needs.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
-
-  If you don't know anything about Lua, I recommend taking some time to read through
-  a guide. One possible example:
-  - https://learnxinyminutes.com/docs/lua/
-
-
   And then you can explore or search through `:help lua-guide`
   - https://neovim.io/doc/user/lua-guide.html
 
@@ -27,15 +9,6 @@ Kickstart Guide:
 I have left several `:help X` comments throughout the init.lua
 You should run that command and read that help section for more information.
 
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now :)
 --]]
 
 -- Set <space> as the leader key
@@ -314,6 +287,17 @@ vim.o.termguicolors = true
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
+
+
+vim.keymap.set({ 'n' }, '<Space>p', [[:w<CR>:!python %<CR>]], { desc = '' })
+vim.keymap.set({ 'n' }, '<Space>t', [[:w<CR>:term python %<CR>]], { desc = '' })
+vim.keymap.set({ 'n' }, '<Space>fs', [[:w<CR>]], { desc = '' })
+vim.keymap.set({ 'n' }, '<Space>bp', [[:bp<CR>]], { desc = '' })
+vim.keymap.set({ 'n' }, '<Space>bn', [[:bn<CR>]], { desc = '' })
+vim.keymap.set({ 'n' }, '<Space>bd', [[:bd<CR>]], { desc = '' })
+
+
+
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
@@ -565,7 +549,7 @@ require('which-key').register({
 local servers = {
   -- clangd = {},
   -- gopls = {},
-  -- pyright = {},
+  pyright = {},
   -- rust_analyzer = {},
   -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
